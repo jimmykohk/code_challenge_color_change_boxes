06ae452fe8835de8bf888e196d07476bd21b90ac
Jimmy KO <jimmyko_kcp@hotmail.com>
2022-04-22 23:15:14 +0800

    First publish

---
+++ color_change_boxes/src/App.vue
@@ -0,0 +1,107 @@
+<script setup>
+import Box from './components/Box.vue'
+</script>
+
+<template>
+  <main>
+    <div class="flex flex-row">
+      <Box 
+        class="col"
+        message="1"
+        ref="box1"
+        @box-clicked="detectBoxClicked" />
+      <div
+        class="col flex flex-col">
+        <Box 
+          ref="box2"
+          message="2"
+          @box-clicked="detectBoxClicked" />
+        <div
+          class="col flex flex-fixed-row flex-mobile-first">
+          <Box
+            class="col" 
+            message="3"
+            ref="box3"
+            @box-clicked="detectBoxClicked" />
+          <Box 
+            class="col"
+            message="4"
+            ref="box4"
+            @box-clicked="detectBoxClicked" />
+        </div>
+      </div>
+    </div>
+
+    <div class="mt-1 flex flex-fixed-row flex-wrap">
+      <div class="col">
+        <div class="flex flex-fixed-col">
+          <Box message="5" ref="box5" @box-clicked="detectBoxClicked" />
+          <Box message="6" ref="box6" @box-clicked="detectBoxClicked" />
+        </div>
+      </div>
+
+      <Box 
+        class="col flex-mobile-first flex-mobile-width-100"
+        message="7"
+        ref="box7"
+        @box-clicked="detectBoxClicked" />
+      <div class="col">
+        <div class="flex flex-fixed-col">
+          <Box message="8" ref="box8" @box-clicked="detectBoxClicked" />
+          <Box message="9" ref="box9" @box-clicked="detectBoxClicked" />
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default{
+  data(){
+    return {
+      boxes: ["box1", "box2", "box3", "box4", "box5", "box6", "box7", "box8", "box9"]
+    }
+  },
+  components: {
+    Box
+  },
+  methods: {
+    detectBoxClicked(){
+      this.boxes.forEach(box=>this.$refs[box].changeBoxColor())
+    }
+  }
+}
+</script>
+
+<style>
+@import './assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+.mt-1 { margin-top: 1rem; }
+
+.flex { 
+  display: flex;
+  gap: 1rem;
+}
+.flex-wrap { flex-wrap: wrap; }
+.flex-nowrap { flex-wrap: nowrap; }
+.flex-row{ flex-direction: row; }
+.flex-col{ flex-direction: column;}
+.flex-fixed-row { flex-direction: row !important; }
+.flex-fixed-col { flex-direction: column !important; }
+.col { flex: 1 }
+
+
+@media (max-width: 600px){
+  .flex-row { flex-direction: column; }
+  .flex-mobile-width-100 { flex-basis: 100%; }
+  .flex-mobile-first { order: -1; }
+}
+</style>
---
+++ color_change_boxes/src/components/Box.vue
@@ -0,0 +1,71 @@
+<template>
+  <div
+    ref="ref1"
+    class="box-container"
+    :style="{ 'background-color': backgroundColor }"
+    @click="boxClicked">
+    <span>{{message}}</span>
+  </div>
+</template>
+
+
+<script>
+import { ref } from 'vue'
+
+export default{
+  props: {
+    message: String
+  },
+  setup(props){
+    const divRef = ref(null)
+
+    // Expose to template
+    return { props, divRef }
+  },
+  data(){
+    return{
+      red: 0,
+      green: 0,
+      blue: 0
+    }
+  },
+  computed: {
+    backgroundColor: function(){
+      return `rgb(${this.red}, ${this.green}, ${this.blue})`
+    }
+  },
+  mounted: function() {
+    this.changeBoxColor()
+  },
+  methods: {
+    boxClicked(){
+      this.$emit("boxClicked")
+    },
+
+    generateRandomColorCode(){
+      return parseInt(Math.random() * 255)
+    },
+
+    changeBoxColor(){
+      this.red = this.generateRandomColorCode()
+      this.green = this.generateRandomColorCode()
+      this.blue = this.generateRandomColorCode()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.box-container{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1em;
+  cursor: pointer;
+  text-align: center;
+}
+span{ 
+  font-weight: bold;
+  color: white;
+}
+</style>
